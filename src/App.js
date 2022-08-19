5ab472eb0831d7a16cab91f3538d4f73dc3849f6
jercarlo30 <jloprodaction80@gmail.com>
2022-08-19 21:08:36 +0700

    Menambahkan Components-menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import MenuMakanan from './Menu/MenuMakanan/menuMakanan';
+import MenuMinuman from './Menu/MenuMinuman/menuMinuman';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <div className='App'>
+        <h1 className='header-campus-se'>Canteen Campus SE</h1>
+        <h2 className='header-menu-makanan'>Menu Makanan</h2>
+        <MenuMakanan/>
+        <h2 className='header-menu-minuman'>Menu Minuman</h2>
+        <MenuMinuman/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Menu/MenuMinuman/menuMinuman.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class menuMinuman extends Component {
+    render() {
+        return(
+            <div style={{border: '1px solid black', width: 300, margin: 'auto'}}>
+                 <p>Nama Menu: Es teh manis</p>
+                <p>Harga: Rp5.000</p>
+            </div>
+        );
+    }
+}
+
+export default menuMinuman;
